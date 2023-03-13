11ef93a43b75431c116fdb7f8554be99022c4700
Adenola desmond <56209529+locksiDesmond@users.noreply.github.com>
2023-03-13 03:56:01 +0100

    chore: enable cors and setup port

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -18,6 +19,9 @@ import { EmployeesModule } from './employees/employees.module';
       entities: [EmployeeEntity],
       synchronize: true,
       dropSchema: true,
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.development.env',
     }),
     EmployeesModule,
   ],
